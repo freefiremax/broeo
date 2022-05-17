876b77c82599fb40aaeefde79ec5b12ff94d2d90
freefiremax <67188362+freefiremax@users.noreply.github.com>
2022-05-17 12:19:31 +0530

    Update details(1).js

--- details(1).js
+++ details(1).js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: '5248631376:AAEq92FV2RvrZvt-Yzs5O39s9gQ3-5Y3KgQ',
-    admins: [1468386562,1821521027],
+    token: '5399014600:AAF8ZyyGJZXeOo11Y8tIInJ6ok_JfPlJ-MM',
+    admins: [5081665130],
     curr:'Paisa'
 }
